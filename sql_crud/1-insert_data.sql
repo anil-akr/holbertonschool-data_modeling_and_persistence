85722e01e8db8da1619111793e612387da1861b1
anil-akr <4nil000@protonmail.com>
2026-06-03 07:24:55 +0000

    T1 fix

--- sql_crud/1-insert_data.sql
+++ sql_crud/1-insert_data.sql
@@ -12,4 +12,4 @@ VALUES
     (2, '1984', 'George Orwell', 'Dystopian', 8.99, 12, 1949),
     (3, 'Clean Code', 'Robert C. Martin', 'Tech', 32.50, 7, 2008),
     (4, 'The Pragmatic Programmer', 'Andrew Hunt', 'Tech', 28.75, 4, 1999),
-    (5, 'Dune', 'Frank Herbert', 'Fantasy', 9.50, 5, 1965);
+    (5, 'Dune', 'Frank Herbert', 'Sci-Fi', 9.50, 5, 1965);
